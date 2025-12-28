0abbc224d82bdf960e55ad9c88f0383a61003c59
ejaz4 <ejazali4@protonmail.ch>
2025-12-28 14:27:26 +0000

    fix(search): set the hook to resolve a certain KV instead of itself directly
    - JS imports JSON files as an object with a key that corresponds with
      the actual interpreted JSON data

--- src/libs/useProperties.js
+++ src/libs/useProperties.js
@@ -2,5 +2,5 @@ import * as PropertiesJSON from "@/assets/properties.json";
 
 // Return the object of the properties
 export const useProperties = () => {
-  return PropertiesJSON;
+  return PropertiesJSON.default;
 };
