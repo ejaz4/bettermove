32d5e26144b063bdb35d8fc9ba9f3e1c1f177ee6
ejaz4 <ejazali4@protonmail.ch>
2026-01-04 20:03:44 +0000

    feat(search): automatically generate fields and dropdowns based on usePropertyFields data

--- src/pages/search.filter.jsx
+++ src/pages/search.filter.jsx
@@ -1,8 +1,28 @@
+import { Modal } from "../_components/modal";
+import { useProperties } from "../libs/useProperties";
+import { usePropertyFields } from "../libs/usePropertyFields";
+
 const FilterModalPage = () => {
+  const fields = usePropertyFields(["id", "tenure", "boundary", "type"]);
+
   return (
-    <div>
-      <p>filter!</p>
-    </div>
+    <Modal>
+      <h2>Filter</h2>
+      <p>Refine the search for your perfect home.</p>
+
+      {Object.keys(fields).map((name) => {
+        return (
+          <div>
+            <p>{name}</p>
+            <select>
+              {fields[name].map((op) => (
+                <option>{op}</option>
+              ))}
+            </select>
+          </div>
+        );
+      })}
+    </Modal>
   );
 };
 
